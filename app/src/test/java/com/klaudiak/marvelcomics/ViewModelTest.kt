e4718093a3d6cf6fd33e3c2c5318624d8a6914df
Klaudia Kowalska <klaudiakowalska@MacBook-Pro-Klaudia.local>
2022-07-15 10:23:22 +0200

    Add get comic list test

--- app/src/test/java/com/klaudiak/marvelcomics/ViewModelTest.kt
+++ app/src/test/java/com/klaudiak/marvelcomics/ViewModelTest.kt
@@ -2,10 +2,13 @@ package com.klaudiak.marvelcomics
 
 import com.klaudiak.marvelcomics.data.models.local.ComicItem
 import com.klaudiak.marvelcomics.domain.ComicRepository
+import com.klaudiak.marvelcomics.presentation.comics_list.ComicListState
 import com.klaudiak.marvelcomics.presentation.comics_list.ComicListViewModel
 import com.klaudiak.marvelcomics.presentation.search_screen.SearchScreenEvent
 import com.klaudiak.marvelcomics.utils.Resource
 import io.mockk.*
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runBlockingTest
@@ -22,24 +25,25 @@ class ComicViewModelTest : TestConfig() {
     lateinit var viewModel: ComicListViewModel
 
 
+    private val comicItem = ComicItem(
+        id = 1,
+        title = "",
+        author = "",
+        description = null,
+        thumbnail = null,
+        uriDetails = ""
+    )
+
     @Before
     fun before() {
-
         comicRepository = mockk()
         viewModel = ComicListViewModel(comicRepository)
     }
 
 
     @Test
-    fun testGetComicById() {
-        val comicItem = ComicItem(
-            id = 1,
-            title = "",
-            author = "",
-            description = null,
-            thumbnail = null,
-            uriDetails = ""
-        )
+    fun `Test get comic by id`() {
+
         val comic = flowOf(
             Resource.Success(
                 comicItem
@@ -52,10 +56,54 @@ class ComicViewModelTest : TestConfig() {
     }
 
     @Test
-    fun testOnQueryChanged() {
+    fun `Test event handling`() {
         val query = "empty"
         viewModel.onEvent(SearchScreenEvent.OnQueryChanged(query))
         assert(viewModel.uiState.value.query == query)
 
+    }
+
+
+    @Test
+    fun `Test check if query changed func not execute`() {
+        val comicItem2 = comicItem.copy(id = 2)
+        val flow_1 = flowOf(
+            Resource.Loading(true),
+            Resource.Success(data = listOf(comicItem)),
+            Resource.Loading(false)
+        )
+        val flow_2 = flowOf(
+            Resource.Loading(true),
+            Resource.Success(data = listOf(comicItem2)),
+            Resource.Loading(false)
+        )
+        coEvery { comicRepository.searchComicList("1") } returns flow_1
+        coEvery { comicRepository.searchComicList("2") } returns flow_2
+
+        runBlocking {
+            viewModel.onEvent(SearchScreenEvent.OnQueryChanged("1"))
+            assert(viewModel.uiState.value.query == "1")
+            viewModel.onEvent(SearchScreenEvent.OnQueryChanged("2"))
+            assert(viewModel.uiState.value.query == "2")
+        }
+        assertNotNull(viewModel.uiState.value.searchedItems)
+        assert(viewModel.uiState.value.searchedItems != listOf(comicItem))
+    }
+
+    @Test
+    fun `Test get comic list`() {
+
+        val flow = flowOf(
+            Resource.Loading(true),
+            Resource.Success(data = listOf(comicItem)),
+        )
+        coEvery { comicRepository.getComicList() } returns flow
+        val getComicRes = comicRepository.getComicList()
+        if (getComicRes is Resource.Success<*>) {
+            assert(viewModel.uiState.value.items == listOf(comicItem))
+            assert(!viewModel.uiState.value.isLoading)
+        } else if (getComicRes is Resource.Loading<*>) {
+            assert(viewModel.uiState.value.isLoading)
+        }
     }
 }
